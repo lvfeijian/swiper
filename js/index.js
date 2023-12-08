c183fc884183c0e3dc3fadcc189f48feb95bc494
挽歌渡临舟 <1538145901@qq.com>
2023-12-08 16:32:37 +0800

    fix：无缝轮播的实现

--- js/index.js
+++ js/index.js
@@ -6,6 +6,11 @@ let currentIndex = 0;
 let imgs = carousel.querySelectorAll('img')
 let timerId = null
 function init(){
+  var first = carousel.firstElementChild.cloneNode(true)
+  var last = carousel.lastElementChild.cloneNode(true)
+  last.style.marginLeft = '-100%'
+  carousel.appendChild(first)
+  carousel.insertBefore(last, carousel.firstElementChild)
   arrow.style.display = 'none'
   container.addEventListener('mouseenter',(e) => {
     timerId && clearInterval(timerId)
@@ -15,21 +20,36 @@ function init(){
     arrow.style.display = 'none'
     autoPlay()
   })
-  arrow.querySelector('.left').onclick = (e)=>{
-    currentIndex = (--currentIndex + imgs.length) % imgs.length
-    moveTo(currentIndex)
-  }
-  arrow.querySelector('.right').onclick = (e)=>{
-    console.log(currentIndex);
-    currentIndex = ++currentIndex % imgs.length
-    moveTo(currentIndex)
-  }
+  arrow.querySelector('.left').onclick = leftClick
+  arrow.querySelector('.right').onclick = rightClick
 }
 function moveTo(index){
+  carousel.style.transition = "0.6s"
   carousel.style.transform = `translateX(${index * -100}%)`;
   let activeClass = document.querySelector('.indicator span.active')
   activeClass.classList.remove('active')
   indicators[index].classList.add('active')
+  currentIndex = index
+}
+function rightClick(){
+  if(currentIndex == imgs.length-1){
+    carousel.style.transform = "translateX(100%)"
+    carousel.style.transition = 'none'
+    carousel.getBoundingClientRect()
+    moveTo(0)
+  } else {
+    moveTo(currentIndex+1)
+  }
+}
+function leftClick(){
+  if(currentIndex == 0){
+    carousel.style.transform = `translateX(-${imgs.length}00%)`
+    carousel.style.transition = 'none'
+    carousel.getBoundingClientRect()
+    moveTo(imgs.length-1)
+  } else {
+    moveTo(currentIndex-1)
+  }
 }
 indicators.forEach((item, i) => {
   item.onclick = function () {
@@ -39,8 +59,7 @@ indicators.forEach((item, i) => {
 })
 function autoPlay(){
   timerId = setInterval(() => {
-    currentIndex = ++currentIndex % imgs.length;
-    moveTo(currentIndex);
+    rightClick()
   }, 2000);
 }
 init()
